4524d6663ebb6be14dab12beca5b803884d664bd
Pozsár Zsolt <pozsarzs@vuk.lan>
2022-03-09 06:29:16 +0100

    220309

--- source/untcommon.pas
+++ source/untcommon.pas
@@ -1,6 +1,6 @@
 { +--------------------------------------------------------------------------+ }
-{ | MM3D v0.7 * Growing house controlling and remote monitoring system       | }
-{ | Copyright (C) 2018-2022 Pozsár Zsolt <pozsar.zsolt@szerafingomba.hu>     | }
+{ | MM8D v0.3 * Growing house and irrigation controlling and monitoring sys. | }
+{ | Copyright (C) 2020-2022 Pozsár Zsolt <pozsar.zsolt@szerafingomba.hu>     | }
 { | untcommon.pas                                                            | }
 { | Common functions and procedures                                          | }
 { +--------------------------------------------------------------------------+ }
@@ -17,12 +17,21 @@ interface
 uses
   crt;
 
+function terminalsize: boolean;
 procedure background;
 procedure footer(ypos: byte; title: string);
 procedure header(title: string);
 procedure quit(halt_code: byte; clear: boolean; message: string);
 
 implementation
+
+// check terminal size
+function terminalsize: boolean;
+begin
+  if (screenwidth>=80) and (screenheight>=25)
+    then terminalsize:=true
+    else terminalsize:=false;
+end;
 
 procedure background;
 begin
@@ -36,7 +45,8 @@ var
   b: byte;
 begin
   textbackground(lightgray); textcolor(black);
-  gotoxy(1,ypos); clreol; gotoxy(2,ypos);
+  gotoxy(1,ypos); clreol;
+  gotoxy(2,ypos);
   for b:=1 to length(title) do
   begin
     case title[b] of
@@ -50,8 +60,6 @@ end;
 
 // write header
 procedure header(title: string);
-var
-  b: byte;
 begin
   textbackground(lightgray); textcolor(black);
   gotoxy(1,1); clreol;
